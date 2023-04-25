cbaabdf937c74c964d10daaedc612489601ef641
KFrankPA <114306682+KFrankPA@users.noreply.github.com>
2023-04-24 23:52:31 -0400

    added OurProject

---
+++ src/OurProject.java
@@ -0,0 +1,19 @@
+public class OurProject {
+
+    public static void main(String[] args) {
+
+
+        String str = "Hello Java";
+
+
+        for (int i = str.length()-1; i >=0 ; i--) {
+
+            System.out.print(str.charAt(i));
+
+        }
+
+
+
+    }
+
+}
